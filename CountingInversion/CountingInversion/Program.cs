b486f2484f0535d0ab8aac53fd7ff167bcb94284
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-08-26 20:32:45 +0800

    AC week 2

---
+++ CountingInversion/CountingInversion/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+
+
+namespace CountingInversion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //int[] input = new int[] { 9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0 };
+            //int[] input = new int[] { 37, 7, 2, 14, 35, 47, 10, 24, 44, 17, 34, 11, 16, 48, 1, 39, 6, 33, 43, 26, 40, 4, 28, 5, 38, 41, 42, 12, 13, 21, 29, 18, 3, 19, 0, 32, 46, 27, 31, 25, 15, 36, 20, 8, 9, 49, 22, 23, 30, 45 };
+            int[] input = ReadInputFromFile();
+
+            long invCount = SortAndCount(ref input);
+
+            Console.WriteLine("Total inversion count = {0}", invCount);
+        }
+
+        private static int[] ReadInputFromFile()
+        {
+            StreamReader file = new StreamReader(@"/Users/phillipzhang/Work/git/AlgorithmCourse/CountingInversion/InputArray.txt");
+
+            string line;
+            int counter = 0;
+            int[] input = new int[100000]; 
+
+            while((line = file.ReadLine()) != null)  
+            {
+                input[counter] = Int32.Parse(line);
+                counter++;  
+            }
+
+            file.Close();
+            Console.WriteLine("Read Input Complete");
+            return input;
+        }
+
+        private static long SortAndCount(ref int[] source)
+        {
+            if (source.Length == 1)
+            {
+                return 0;
+            }
+
+            int halfLength = source.Length / 2;
+            int[] firstHalf = new int[halfLength];
+            int[] secondHalf = new int[source.Length - halfLength];
+
+            for (int i = 0; i < halfLength; i++)
+            {
+                firstHalf[i] = source[i];
+            }
+            for (int i = 0; i < source.Length - halfLength; i++)
+            {
+                secondHalf[i] = source[i + halfLength];
+            }
+
+            long x = SortAndCount(ref firstHalf);
+            long y = SortAndCount(ref secondHalf);
+            long z = MergeAndCountSplitInv(firstHalf, secondHalf, ref source);
+
+            return x + y + z;
+        }
+
+        private static long MergeAndCountSplitInv(int[] firstHalf, int[] secondHalf, ref int[] source)
+        {
+            int[] merge = new int[firstHalf.Length + secondHalf.Length];
+
+            long invCount = 0;
+            int i = 0, j = 0;
+            for (int k = 0; k < merge.Length; k++)
+            {
+                if (i >= firstHalf.Length)
+                {
+                    merge[k] = secondHalf[j];
+                    j++;
+                    continue;
+                }
+
+                if (j >= secondHalf.Length)
+                {
+                    merge[k] = firstHalf[i];
+                    i++;
+                    continue;
+                }
+
+
+                if (firstHalf[i] <= secondHalf[j])
+                {
+                    merge[k] = firstHalf[i];
+                    i++;
+                }
+                else
+                {
+                    merge[k] = secondHalf[j];
+                    j++;
+                    invCount += firstHalf.Length - i;
+                }
+            }
+            source = merge;
+
+
+            //for (int x = 0; x < firstHalf.Length; x++)
+            //{
+            //    Console.WriteLine("firstHalf[{0}] = {1}", x, firstHalf[x]);
+            //}
+            //for (int x = 0; x < secondHalf.Length; x++)
+            //{
+            //    Console.WriteLine("secondHalf[{0}] = {1}", x, secondHalf[x]);
+            //}
+            //for (int x = 0; x < merge.Length; x++)
+            //{
+            //    Console.WriteLine("merge[{0}] = {1}", x, merge[x]);
+            //}
+
+            //Console.WriteLine("invCount = {0}", invCount);
+
+            return invCount;
+        }
+    }
+}
--- KaratsubaMultiplication/KaratsubaMultiplication/Program.cs
+++ KaratsubaMultiplication/KaratsubaMultiplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+
 
 namespace KaratsubaMultiplication
 {
@@ -6,7 +8,58 @@ namespace KaratsubaMultiplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string xString = "3141592653589793238462643383279502884197169399375105820974944592";
+            string yString = "2718281828459045235360287471352662497757247093699959574966967627";
+            BigInteger x = BigInteger.Parse(xString);
+            BigInteger y = BigInteger.Parse(yString);
+
+            Console.WriteLine("Karatsuba Multiplication: {0} * {1}", x, y);
+
+            BigInteger result = Karatsuba(x, y);
+
+            Console.WriteLine("Result  = {0}", result);
+            Console.WriteLine("Should be {0}", x * y);
+        }
+
+        private static BigInteger Karatsuba(BigInteger x, BigInteger y)
+        {
+            Console.WriteLine("________________________");
+            Console.WriteLine("x = {0}, y = {1}", x, y);
+
+            if (x < 10 || y < 10)
+            {
+                BigInteger result = x * y;
+                Console.WriteLine("return {0}", result);
+                return result;
+            }
+
+            BigInteger xDigitsHalfPow10 = BigInteger.Pow(10, (int)((Math.Floor(BigInteger.Log10(x) + 1)) / 2));
+            BigInteger yDigitsHalfPow10 = BigInteger.Pow(10, (int)((Math.Floor(BigInteger.Log10(y) + 1)) / 2));
+
+            Console.WriteLine("xDigitsHalfPow10 = {0}, yDigitsHalfPow10 = {1}", xDigitsHalfPow10, yDigitsHalfPow10);
+
+            // We cannot use Katsuba Multiplication when x & y have different num of digits!
+            if (xDigitsHalfPow10 != yDigitsHalfPow10)
+            {
+                BigInteger result = x * y;
+                Console.WriteLine("Different digits, cannot use Katsuba Multiplicaiton, return {0}", result);
+                return result;
+            }
+
+
+            BigInteger a = x / xDigitsHalfPow10;
+            BigInteger b = x % xDigitsHalfPow10;
+            BigInteger c = y / yDigitsHalfPow10;
+            BigInteger d = y % yDigitsHalfPow10;
+
+            Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}", a, b, c, d);
+
+            BigInteger ac = Karatsuba(a, c);
+            BigInteger bd = Karatsuba(b, d);
+            BigInteger step3 = Karatsuba((a + b), (c + d));
+            BigInteger step4 = step3 - ac - bd;
+
+            return ac * xDigitsHalfPow10 * yDigitsHalfPow10 + bd + step4 * xDigitsHalfPow10;
         }
     }
 }
